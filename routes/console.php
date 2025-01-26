2c3b90d30acb801f4c1cf935ab29833332ff447c
Sven van Ginkel <svenvanginkel@icloud.com>
2025-01-26 18:11:08 +0100

    Change name

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Actions\CheckForScheduledSpeedtests;
-use App\Actions\Notifications\CheckAndSendDailyAverageNotifications;
-use App\Actions\Notifications\CheckAndSendWeeklyAverageNotifications;
+use App\Actions\Notifications\SendDailyAverageNotifications;
+use App\Actions\Notifications\SendWeeklyAverageNotifications;
 use Illuminate\Support\Facades\Schedule;
 
 /**
@@ -40,10 +40,10 @@ Schedule::everyMinute()
 
 Schedule::daily()
     ->group(function () {
-        Schedule::call(fn () => CheckAndSendDailyAverageNotifications::run());
+        Schedule::call(fn () => SendDailyAverageNotifications::run());
     });
 
 Schedule::weeklyOn(1, '00:00')
     ->group(function () {
-        Schedule::call(fn () => CheckAndSendWeeklyAverageNotifications::run());
+        Schedule::call(fn () => SendWeeklyAverageNotifications::run());
     });
