d50044e43806a6c52722507c208adf0b4c4dd956
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 23:48:29 +0100

    New translations thresholds.php (Dutch)

---
+++ lang/nl_NL/settings/thresholds.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'title' => 'Drempels',
+    'label' => 'Drempels',
+
+    // Absolute thresholds
+    'absolute' => 'Absoluut',
+    'absolute_description' => 'Absolute drempels houden geen rekening met de voorgeschiedenis en kunnen bij elke test in werking treden.',
+    'absolute_enabled' => 'Absolute drempels inschakelen',
+    'absolute_download' => 'downloaden',
+    'absolute_upload' => 'Uploaden',
+    'absolute_ping' => 'Ping',
+
+    // Metrics section
+    'metrics' => 'Statistieken',
+    'metrics_helper_text' => 'Ingesteld op nul om deze meting uit te schakelen.',
+
+    // Units
+    'mbps' => 'Mbps',
+    'ms' => 'ms',
+
+    // General threshold labels
+    'thresholds' => 'Drempels',
+    'threshold_enabled' => 'Drempel ingeschakeld',
+    'threshold_download' => 'Drempel downloaden',
+    'threshold_upload' => 'Drempel uploaden',
+    'threshold_ping' => 'Drempel ping',
+];
