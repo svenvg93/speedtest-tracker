7aced3efe134e1fac2d1babe2047f7d518c1eb20
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 23:48:22 +0100

    New translations errors.php (Dutch)

---
+++ lang/nl_NL/errors.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'server_error' => 'Server fout',
+    'oops_server_error' => 'Oeps, server fout!',
+    'error_message' => 'Foutmelding',
+
+    // Speedtest specific errors
+    'ookla_error' => 'Er is een fout opgetreden tijdens het weergeven van snelheidstest servers, controleer de logbestanden.',
+    'cron_invalid' => 'Ongeldige cron expressie',
+
+    // Status fix command
+    'status_fix' => [
+        'confirm' => 'Wilt u doorgaan?',
+        'fail' => 'Commando afgebroken.',
+        'finished' => '✅ klaar!',
+        'info_1' => 'Dit zal alle resultaten controleren en de status herstellen naar "voltooid" of "mislukt" op basis van de gegevens.',
+        'info_2' => '📖 Lees de documentatie: https://docs.speedtest-tracker.dev/other/commando\'s',
+    ],
+];
