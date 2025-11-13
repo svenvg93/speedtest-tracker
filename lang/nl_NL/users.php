2fcf3e2121d13e004698947970ee5e702808b445
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 23:48:26 +0100

    New translations users.php (Dutch)

---
+++ lang/nl_NL/users.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'title' => 'Gebruikers',
+    'label' => 'Gebruikers',
+
+    // User management
+    'user' => 'Gebruiker',
+    'users' => 'Gebruikers',
+    'admin' => 'Beheerder',
+    'role' => 'Functie',
+
+    // User prompts and messages
+    'user_change' => [
+        'info' => 'Gebruikersrol bijgewerkt.',
+        'password_updated_info' => ':email wachtwoord bijgewerkt.',
+        'what_is_password' => 'Wat is het nieuwe wachtwoord?',
+        'what_is_the_email_address' => 'Wat is het e-mailadres?',
+        'what_role' => 'Welke rol moet de gebruiker hebben?',
+    ],
+];
