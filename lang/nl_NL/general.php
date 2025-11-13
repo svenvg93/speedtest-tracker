d3eeb91c98a958f4289db2c4e806a07cbc1bffc1
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 23:48:23 +0100

    New translations general.php (Dutch)

---
+++ lang/nl_NL/general.php
@@ -0,0 +1,69 @@
+<?php
+
+return [
+    // Common actions
+    'save' => 'Opslaan',
+    'cancel' => 'annuleren',
+    'delete' => 'Verwijderen',
+    'edit' => 'Bewerken',
+    'create' => 'Aanmaken',
+    'search' => 'Zoeken',
+    'filter' => 'Filteren',
+    'export' => 'Exporteren',
+    'actions' => 'acties',
+    'enable' => 'Inschakelen',
+    'yes' => 'Ja',
+    'no' => 'Neen',
+    'options' => 'Instellingen',
+    'details' => 'Beschrijving',
+
+    // Common labels
+    'name' => 'Naam',
+    'email' => 'E-mailadres',
+    'email_address' => 'E-mail adres',
+    'password' => 'Wachtwoord',
+    'password_confirmation' => 'Wachtwoord bevestiging',
+    'id' => 'ID',
+    'status' => 'status',
+    'message' => 'Bericht',
+    'comment' => 'Opmerking',
+    'comments' => 'Opmerkingen',
+    'created_at' => 'Aangemaakt op',
+    'updated_at' => 'Bijgewerkt op',
+    'url' => 'URL',
+
+    // Navigation
+    'dashboard' => 'Dashboard',
+    'results' => 'Resultaten',
+    'settings' => 'Instellingen',
+    'users' => 'Gebruikers',
+    'documentation' => 'Documentatie',
+    'links' => 'Koppelingen',
+    'donate' => 'Doneren',
+
+    // Roles
+    'admin' => 'Beheerder',
+    'user' => 'Gebruiker',
+    'role' => 'Functie',
+
+    // Date ranges
+    'last_24h' => 'Afgelopen 24 uur',
+    'last_week' => 'Vorige week',
+    'last_month' => 'Vorige maand',
+
+    // Metrics
+    'average' => 'Gemiddeld',
+    'high' => 'hoog',
+    'low' => 'laag',
+    'faster' => 'Sneller',
+    'slower' => 'Langzamer',
+    'healthy' => 'Gezond',
+
+    // Application
+    'speedtest_tracker' => 'Snelheids Tracker',
+    'platform' => 'Platform',
+
+    // Update status
+    'update_available' => 'Update beschikbaar!',
+    'up_to_date' => 'Bijgewerkt tot',
+];
