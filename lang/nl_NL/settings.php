25d5dfece84da4ac6f45327bbf3b643c0c130315
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 23:48:25 +0100

    New translations settings.php (Dutch)

---
+++ lang/nl_NL/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title' => 'Instellingen',
+    'label' => 'Instellingen',
+
+    // Common settings labels
+    'triggers' => 'Triggeraars',
+    'verify_ssl' => 'Controleer SSL',
+    'username' => 'Gebruikersnaam',
+    'username_placeholder' => 'Gebruikersnaam voor Basis Auth (optioneel)',
+    'password_placeholder' => 'Wachtwoord voor basis authenticatie (optioneel)',
+];
