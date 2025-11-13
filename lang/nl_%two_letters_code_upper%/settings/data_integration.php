ffda8a1a6fb7ee5976cbba5b59b1f523dd087b85
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 23:41:51 +0100

    New translations data_integration.php (Dutch)

--- lang/nl_%two_letters_code_upper%/settings/data_integration.php
+++ lang/nl_%two_letters_code_upper%/settings/data_integration.php
@@ -1,29 +1,29 @@
 <?php
 
 return [
-    'title' => 'Data Integration',
-    'label' => 'Data Integration',
+    'title' => 'Data integratie',
+    'label' => 'Data integratie',
 
     // InfluxDB v2
     'influxdb_v2' => 'InfluxDB v2',
-    'influxdb_v2_description' => 'When enabled, all new Speedtest results will also be sent to InfluxDB.',
-    'influxdb_v2_enabled' => 'Enable',
+    'influxdb_v2_description' => 'Wanneer ingeschakeld, zullen alle nieuwe Snelheids resultaten ook worden verzonden naar de InfluxDB.',
+    'influxdb_v2_enabled' => 'Inschakelen',
     'influxdb_v2_url' => 'URL',
     'influxdb_v2_url_placeholder' => 'http://your-influxdb-instance',
     'influxdb_v2_org' => 'Org',
-    'influxdb_v2_bucket' => 'Bucket',
-    'influxdb_v2_bucket_placeholder' => 'speedtest-tracker',
+    'influxdb_v2_bucket' => 'Emmer',
+    'influxdb_v2_bucket_placeholder' => 'snelheid-tracker',
     'influxdb_v2_token' => 'Token',
-    'influxdb_v2_verify_ssl' => 'Verify SSL',
+    'influxdb_v2_verify_ssl' => 'Controleer SSL',
 
     // Actions
-    'export_current_results' => 'Export current results',
-    'test_connection' => 'Test connection',
-    'starting_bulk_data_write_to_influxdb' => 'Starting bulk data write to InfluxDB',
-    'sending_test_data_to_influxdb' => 'Sending test data to InfluxDB',
+    'export_current_results' => 'Huidige resultaten exporteren',
+    'test_connection' => 'Verbindingstest testen',
+    'starting_bulk_data_write_to_influxdb' => 'bulkgegevens schrijven naar InfluxDB starten',
+    'sending_test_data_to_influxdb' => 'Testgegevens verzenden naar InfluxDB',
 
     // Common labels (can be removed if they're in general.php)
     'org' => 'Org',
-    'bucket' => 'Bucket',
+    'bucket' => 'Emmer',
     'token' => 'Token',
 ];
