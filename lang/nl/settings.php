a572b6db04898149537f04e7f07c28cf31850166
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 20:55:31 +0100

    New translations settings.php (Dutch)

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -10,103 +10,11 @@ return [
     |
     */
 
-    'bucket' => 'Emmer',
-    'data_integration' => 'Data integratie',
-    'enable_influxdb' => 'InfluxDB integratie inschakelen',
-    'influxdb' => 'InfluxDB',
-    'influxdb_description' => 'Configureer InfluxDB integratie instellingen.',
-    'organization' => 'Rekening',
-    'settings' => 'Instellingen',
-    'url' => 'URL',
-    'verify_ssl' => 'Controleer SSL Certificaat',
-
-    // Notification Settings
-    'notifications' => 'Notificaties',
-    'triggers' => 'Triggeraars',
-    'notify_on_every_speedtest_run' => 'Notificatie bij elke snelheidstest uitgevoerd',
-    'notify_on_threshold_failures' => 'Melding bij drempelfouten',
-
-    // Database Notifications
-    'database' => 'Database',
-    'database_description' => 'Instellingen database meldingen configureren.',
-    'enable_database_notifications' => 'Database notificaties inschakelen',
-    'test_database_channel' => 'Test database kanaal',
-
-    // Email Notifications
-    'mail' => 'E-mailen',
-    'enable_mail_notifications' => 'Schakel e-mailnotificaties in',
-    'recipients' => 'Ontvangers',
-    'email_address' => 'Uw e-mailadres',
-    'test_mail_channel' => 'Test Mail Kanaal',
-
-    // Webhook Notifications
-    'webhook' => 'Webhook',
-    'webhooks' => 'Webhooks',
-    'enable_webhook_notifications' => 'Webhook meldingen inschakelen',
-    'test_webhook_channel' => 'Test Webhook kanaal',
-
-    // Discord
-    'discord' => 'Onenigheid',
-    'enable_discord_webhook_notifications' => 'Discord Webhook meldingen inschakelen',
-    'test_discord_webhook' => 'Test Discord Webhook',
-
-    // Gotify
-    'gotify' => 'Doelstelling',
-    'gotify_enabled' => 'Gotify meldingen inschakelen',
-    'test_gotify_webhook' => 'Test Gotify Webhook',
-
-    // Healthchecks.io
-    'healthcheck_io' => 'Healthchecks.io',
-    'healthcheck_enabled' => 'Inschakelen Healthchecks.io meldingen',
-    'test_healthcheck_webhook' => 'Test Healthchecks.io Webhook',
-
-    // Ntfy
-    'ntfy' => 'N.B.',
-    'ntfy_enabled' => 'Ntfy meldingen inschakelen',
-    'test_ntfy_webhook' => 'Test Webhook',
-    'your_ntfy_server_url' => 'Uw Ntfy Server URL',
-    'your_ntfy_topic' => 'Uw Ntfy Onderwerp',
-    'topic' => 'Onderwerp',
-    'username' => 'Gebruikersnaam',
-    'username_placeholder' => 'Gebruikersnaam (optioneel)',
-    'password' => 'Wachtwoord',
-    'password_placeholder' => 'Wachtwoord (optioneel)',
-
-    // Pushover
-    'pushover' => 'Pushover',
-    'pushover_webhooks' => 'Pushover Webhooks',
-    'enable_pushover_webhook_notifications' => 'Pushover meldingen inschakelen',
-    'test_pushover_webhook' => 'Pushover Webhook testen',
-    'your_pushover_api_token' => 'Uw Pushover API Token',
-    'your_pushover_user_key' => 'Uw Pushover Gebruiker Sleutel',
-    'user_key' => 'Gebruiker Sleutel',
-
-    // Slack
-    'slack' => 'Slack',
-    'slack_enabled' => 'Slack meldingen inschakelen',
-    'test_slack_webhook' => 'Slack Webhook testen',
-
-    // Telegram
-    'telegram' => 'Telegram',
-    'enable_telegram' => 'Telegram meldingen inschakelen',
-    'telegram_disable_notification' => 'Notificatie uitschakelen',
-    'test_telegram_webhook' => 'Telegram testen',
-
-    // API Token
-    'api_token' => 'API Token',
-    'options' => 'Instellingen',
-
     // Thresholds Settings
     'thresholds' => 'Drempels',
-    'threshold_helper_text' => 'Stel drempelwaarden in voor snelheidste-resultaten. Laat leeg om uit te schakelen.',
     'enable_absolute_thresholds' => 'Absolute drempels inschakelen',
     'absolute_thresholds_description' => 'Absolute drempels houden geen rekening met de voorgeschiedenis en kunnen bij elke test in werking treden.',
     'download' => 'downloaden',
     'upload' => 'Uploaden',
     'ping' => 'Ping',
-
-    // Data Integration
-    'export_current_results' => 'Exporteer huidige resultaten',
-    'test_connection' => 'Verbinding testen',
-
 ];
