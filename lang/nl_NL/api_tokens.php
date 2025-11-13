aede4f6788a54869a25c6df77181dc31faa952a6
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 23:48:20 +0100

    New translations api_tokens.php (Dutch)

---
+++ lang/nl_NL/api_tokens.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'title' => 'API Tokens',
+    'label' => 'API Tokens',
+
+    // Token management
+    'api_token' => 'API token',
+    'api_tokens' => 'API tokens',
+    'create_api_token' => 'API-sleutel aanmaken',
+    'token' => 'Token',
+    'your_token' => 'Jouw token',
+    'token_created' => 'Token aangemaakt',
+    'token_status' => 'Token status',
+
+    // Token lists
+    'active_tokens' => 'Actieve tokens',
+    'expired_tokens' => 'Verlopen tokens',
+    'all_tokens' => 'Alle tokens',
+
+    // Token properties
+    'expires_at' => 'Verloopt op',
+    'expires_at_helper_text' => 'Laat leeg als u geen vervaldatum wilt',
+    'last_used_at' => 'Laatst gebruikt op',
+
+    // Abilities/Permissions
+    'abilities' => 'Vaardigheden',
+    'read_results' => 'Lees resultaten',
+    'read_results_description' => 'De token zal toestemming hebben om resultaten en statistieken te lezen.',
+    'run_speedtest' => 'Snelheidstest uitvoeren',
+    'run_speedtest_description' => 'Het token zal toestemming hebben om snelheidstest uit te voeren.',
+    'list_servers' => 'Toon servers',
+    'list_servers_description' => 'De token zal toestemming hebben om servers weer te geven.',
+];
