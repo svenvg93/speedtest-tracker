96e2a429637d2fa92e3a2700c156210c7ab70736
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 19:42:56 +0100

    New translations passwords.php (Dutch)

--- lang/nl/passwords.php
+++ lang/nl/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Uw wachtwoord is opnieuw ingesteld.',
+    'sent' => 'We hebben uw wachtwoord reset link verzonden.',
+    'throttled' => 'Wacht alsjeblieft voordat je opnieuw probeert.',
+    'token' => 'Dit wachtwoord reset token is ongeldig.',
+    'user' => "We kunnen geen gebruiker met dat e-mailadres vinden.",
 
 ];
