6d7f92e4e560963e898f3798c2d03a878869f9d3
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 23:48:27 +0100

    New translations validation.php (Dutch)

---
+++ lang/nl_NL/validation.php
@@ -0,0 +1,91 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes' => [
+        'address' => 'Adres',
+        'age' => 'leeftijd',
+        'body' => 'inhoud',
+        'cell' => 'cel',
+        'city' => 'stad',
+        'country' => 'Land',
+        'date' => 'datum',
+        'day' => 'Dag',
+        'excerpt' => 'summary',
+        'first_name' => 'voornaam',
+        'gender' => 'geslacht',
+        'marital_status' => 'burgerlijke staat',
+        'profession' => 'beroep',
+        'nationality' => 'nationaliteit',
+        'hour' => 'uur',
+        'last_name' => 'Achternaam is vereist.',
+        'message' => 'Bericht',
+        'minute' => 'minuut',
+        'mobile' => 'mobiel',
+        'month' => 'maand',
+        'name' => 'Naam',
+        'zipcode' => 'postcode',
+        'company_name' => 'Naam bedrijf',
+        'neighborhood' => 'wijk',
+        'number' => 'nummer',
+        'password' => 'Wachtwoord',
+        'phone' => 'telefoon',
+        'second' => 'seconde',
+        'sex' => 'geslacht',
+        'state' => 'staat',
+        'street' => 'straat',
+        'subject' => 'onderwerp',
+        'text' => 'Tekst',
+        'time' => 'Tijd',
+        'title' => 'Titel',
+        'username' => 'gebruikersnaam',
+        'year' => 'Jaar',
+        'description' => 'beschrijving',
+        'password_confirmation' => 'wachtwoord bevestiging',
+        'current_password' => 'huidig wachtwoord',
+        'complement' => 'complementair',
+        'modality' => 'modaliteit',
+        'category' => 'categorie',
+        'blood_type' => 'bloed type',
+        'birth_date' => 'geboortedatum',
+    ],
+];
