fb342cf54d00f0b15860a0b85e0b761a1a9f555f
Sven van Ginkel <svenvanginkel@icloud.com>
2025-11-13 23:48:24 +0100

    New translations results.php (Dutch)

---
+++ lang/nl_NL/results.php
@@ -0,0 +1,76 @@
+<?php
+
+return [
+    'title' => 'Resultaten',
+    'result_overview' => 'Overzicht van resultaten',
+
+    // Metrics
+    'download' => 'downloaden',
+    'download_mbps' => 'Downloaden (Mbps)',
+    'download_ms' => 'Downloaden (ms)',
+    'download_latency' => 'Download latentie',
+    'download_latency_high' => 'latency hoog downloaden',
+    'download_latency_low' => 'Download latency laag',
+    'download_latency_iqm' => 'latency IQM downloaden',
+    'download_latency_jitter' => 'Download latency jitter',
+
+    'upload' => 'Uploaden',
+    'upload_mbps' => 'Upload (Mbps)',
+    'upload_ms' => 'Uploaden (ms)',
+    'upload_latency' => 'Upload latentie',
+    'upload_latency_high' => 'Upload latentie hoog',
+    'upload_latency_low' => 'Upload latency laag',
+    'upload_latency_iqm' => 'Upload latency IQM',
+    'upload_latency_jitter' => 'Upload latency Jitter',
+
+    'ping' => 'Ping',
+    'ping_ms' => 'Ping (ms)',
+    'ping_details' => 'Ping details',
+    'ping_jitter' => 'Ping jitter',
+    'ping_high' => 'Ping hoog',
+    'ping_low' => 'Ping laag',
+
+    'jitter' => 'Jitter',
+    'packet_loss' => 'Pakket verlies',
+    'iqm' => 'IQM',
+    'ms' => 'ms',
+    'average_ms' => 'Gemiddelde (ms)',
+    'high_ms' => 'Hoog (ms)',
+    'low_ms' => 'Laag (ms)',
+
+    // Server & metadata
+    'server_&_metadata' => 'Server & Metadata',
+    'server_id' => 'Server ID',
+    'server_host' => 'Server host',
+    'server_name' => 'Server naam',
+    'server_location' => 'Server locatie',
+    'service' => 'Diensten',
+    'isp' => 'Internetprovider',
+    'ip_address' => 'IP adres',
+    'scheduled' => 'Gepland',
+
+    // Filters
+    'only_healthy_speedtests' => 'Alleen gezonde snelheidstesten',
+    'only_unhealthy_speedtests' => 'Alleen ongezonde snelheidstesten',
+    'only_manual_speedtests' => 'Alleen handmatige snelheden',
+    'only_scheduled_speedtests' => 'Alleen geplande snelheden',
+    'created_from' => 'Aangemaakt vanaf',
+    'created_until' => 'Gemaakt tot',
+
+    // Export
+    'export_all_results' => 'Alle resultaten exporteren',
+    'export_all_results_description' => 'Zal elke kolom exporteren voor alle resultaten.',
+    'export_current_results' => 'Huidige resultaten exporteren',
+    'export_completed' => 'Exporteren voltooid, :count :rows geëxporteerd.',
+    'failed_export' => ':count :rows kon niet exporteren.',
+    'row' => '{1} :count rij [2,*] :count rijen',
+
+    // Actions
+    'update_comments' => 'Reacties bijwerken',
+    'truncate_results' => 'Afkappen resultaten',
+    'truncate_results_description' => 'Weet je zeker dat je alle resultaten wilt afbreken? Deze actie is onomkeerbaar.',
+    'view_on_speedtest_net' => 'Bekijk op Speedtest.net',
+
+    // Error messages
+    'error_message' => 'Foutmelding',
+];
